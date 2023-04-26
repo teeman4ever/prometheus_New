04c9074acf4d4ede81adf4add6819e3c2eecc49d
Tola Awonusi <tola.awonusi@yahoo.com>
2023-04-26 14:34:34 +0100

    Added the gauge metrics to the histogram metrics to depict a multiple metrics scenario.

--- Application Instrumentation/histogram_metric.py
+++ Application Instrumentation/histogram_metric.py
@@ -1,12 +1,14 @@
 import http.server
 import time
-from prometheus_client import start_http_server, Histogram
+from prometheus_client import start_http_server, Histogram, Gauge
 
 REQUEST_LATENCY_TIME = Histogram('request_latency_time', 'Response Latency in Seconds')
+REQUEST_IN_PROGRESS = Gauge('requests_inprogress', "Number of Live Request on Application")
 
 class HandleRequests(http.server.BaseHTTPRequestHandler):
 
     @REQUEST_LATENCY_TIME.time()
+    @REQUEST_IN_PROGRESS.track_inprogress()
     def do_GET(self):
         #startTime = time.time()
         self.send_response(200)
